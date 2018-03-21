611429d8106709c6133e66c441df0916bced80d3
liuxingwei <liuxingwei@rongcapital.cn>
2018-03-21 08:41:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require("express");
+
+let app = express();
+
+let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+
+app.set('port', 3000);
+
+app.use(function(req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:3000; press Ctrl-C to terminate.');
+});
